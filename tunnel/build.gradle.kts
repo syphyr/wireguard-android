365096a88bb230460a84afb62bc7fcb8b7810c51
syphyr <syphyr@gmail.com>
2025-01-15 21:44:32 +0100

    Only build arm64

--- tunnel/build.gradle.kts
+++ tunnel/build.gradle.kts
@@ -20,6 +20,9 @@ android {
     namespace = "${pkg}.tunnel"
     defaultConfig {
         minSdk = 21
+        ndk {
+            abiFilters += listOf("arm64-v8a")
+        }
     }
     externalNativeBuild {
         cmake {
