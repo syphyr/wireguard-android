cd338b12f111353d0b19ac2e3facc1623391906a
syphyr <syphyr@gmail.com>
2025-07-18 21:46:07 +0200

    Update to NDK 27d

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 android {
     compileSdk = 36
-    ndkVersion = "27.2.12479018"
+    ndkVersion = "27.3.13750724"
     buildFeatures {
         buildConfig = true
         dataBinding = true
