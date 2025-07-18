5ce7eae282c2c46a9904949bc667f026e9d41a8a
syphyr <syphyr@gmail.com>
2025-07-18 21:46:07 +0200

    Update to NDK 27d

--- tunnel/build.gradle.kts
+++ tunnel/build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 android {
     compileSdk = 36
-    ndkVersion = "27.2.12479018"
+    ndkVersion = "27.3.13750724"
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
